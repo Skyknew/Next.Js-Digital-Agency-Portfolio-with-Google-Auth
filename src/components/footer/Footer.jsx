dcfb0d5d7820c1a82f25631b25338bc63f32bf2a
Aakash Thapa Chhetri (Skyknew) <77266633+Skyknew@users.noreply.github.com>
2023-06-13 21:24:25 +0545

    Clear the error in footer due to div section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <div>©2023 SkyKnew. All rights reserved.</div>
-      <div className='styles.social'> 
+      <div className={styles.social}> 
       <Image src="/1.png" width={15} height={15} className={styles.icon} alt='SkyKnew Facebook'/>
       <Image src="/2.png" width={15} height={15} className={styles.icon} alt='SkyKnew Pin'/>
       <Image src="/3.png" width={15} height={15} className={styles.icon} alt='SkyKnew Twitter'/>
