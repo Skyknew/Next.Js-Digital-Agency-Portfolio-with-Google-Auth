3b8e8c2d7aa2e3d30eea6bc8c0a382c3eb11c730
Aakash Thapa Chhetri (Skyknew) <77266633+Skyknew@users.noreply.github.com>
2023-06-13 13:52:35 +0545

    added footer desc and icons

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from "./footer.module.css";
+import Image from "next/image";
+
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div>©2023 SkyKnew. All rights reserved.</div>
+      <div className='styles.social'> 
+      <Image src="/1.png" width={15} height={15} className={styles.icon} alt='SkyKnew Facebook'/>
+      <Image src="/2.png" width={15} height={15} className={styles.icon} alt='SkyKnew Pin'/>
+      <Image src="/3.png" width={15} height={15} className={styles.icon} alt='SkyKnew Twitter'/>
+      <Image src="/4.png" width={15} height={15} className={styles.icon} alt='SkyKnew Youtube'/>        
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
